c7b03eb8c3e88ca215f89590c74a1db6131b39bc
Sriram G <marirs@gmail.com>
2021-10-15 15:42:44 +0530

    errors.rs updated

--- libs/disassemblers/smda/src/error.rs
+++ libs/disassemblers/smda/src/error.rs
@@ -1,8 +1,8 @@
 #[derive(thiserror::Error, Debug)]
 pub enum Error {
-    #[error("logic error")]
+    #[error("logic error: {0} - {1}")]
     LogicError(&'static str, u32),
-    #[error("not enough bytes in buffer")]
+    #[error("not enough bytes in buffer: {0} - {1}")]
     NotEnoughBytesError(u64, u64),
     #[error("pe base address error")]
     PEBaseAddressError,
@@ -10,25 +10,26 @@ pub enum Error {
     UnsupportedPEBitnessIDError(u16),
     #[error("invalid rule: {0} - {1}")]
     InvalidRule(u32, String),
-    #[error("json parse error: {0}")]
-    JsonParseError(#[from] serde_json::Error),
     #[error("json format error: {0} - {1}")]
     JsonFormatError(&'static str, u32),
-    #[error("parse int error: {0}")]
-    ParseIntError(#[from] std::num::ParseIntError),
     #[error("operand error")]
     OperandError,
     #[error("collision error: {0}")]
     CollisionError(u64),
-    #[error("utf convert error: {0}")]
-    Utf8Error(#[from] std::str::Utf8Error),
     #[error("dereference error: {0}")]
     DereferenceError(u64),
-    #[error("{0}")]
-    FromHexError(#[from] hex::FromHexError),
 
     #[error("{0}")]
     FromSliceError(#[from] std::array::TryFromSliceError),
+    #[error("utf convert error: {0}")]
+    Utf8Error(#[from] std::str::Utf8Error),
+    #[error("parse int error: {0}")]
+    ParseIntError(#[from] std::num::ParseIntError),
+
+    #[error("{0}")]
+    FromHexError(#[from] hex::FromHexError),
+    #[error("json parse error: {0}")]
+    JsonParseError(#[from] serde_json::Error),
     #[error("{0}")]
     CapstoneError(capstone::Error),
     #[error("{0}")]
