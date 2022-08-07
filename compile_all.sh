92e4f3ba49fd6d4ffa5e04030194d55ebb922019
Sriram Govindan <marirs@gmail.com>
2022-08-07 09:53:18 +0530

    update

--- compile_all.sh
+++ compile_all.sh
@@ -1,13 +1,35 @@
 #!/bin/bash
 
-cargo b --release
-cargo b --release --target x86_64-apple-darwin
-cargo b --release --target x86_64-pc-windows-gnu
-cargo b --release --target aarch64-unknown-linux-gnu
-cargo b --release --target x86_64-unknown-linux-gnu
-cargo b --release --target i686-pc-windows-gnu
+CURRENT_VER=$(head Cargo.toml | grep version | cut -f2 -d'=' | cut -f2 -d\")
 
-cp target/release/auto-ioc dist/auto-ioc-apple-aarch64
-cp target/x86_64-apple-darwin/release/auto-ioc dist/auto-ioc-apple-x86_64
-cp target/x86_64-unknown-linux-gnu/release/auto-ioc dist/auto-ioc-linux-x86_64
-cp target/aarch64-unknown-linux-gnu/release/auto-ioc dist/auto-ioc-linux-aarch64
+# current M1 (aarch) apple binary
+cargo b --release --example capa_cli
+
+# rest of the platforms binary
+cargo b --release --example capa_cli --target x86_64-apple-darwin
+cargo b --release --example capa_cli --target x86_64-pc-windows-gnu
+cargo b --release --example capa_cli --target aarch64-unknown-linux-gnu
+cargo b --release --example capa_cli --target x86_64-unknown-linux-gnu
+
+# remove existing files
+rm -f tmp/*
+
+# copy files to the tmp folder
+# win
+cp target/x86_64-pc-windows-gnu/release/examples/capa_cli.exe tmp/capa_cli_x86-64.exe
+# macos
+cp target/release/examples/capa_cli tmp/capa_macos_aarch64
+cp target/x86_64-apple-darwin/release/examples/capa_cli tmp/capa_macos_x86-64
+# linux
+cp target/aarch64-unknown-linux-gnu/release/examples/capa_cli tmp/capa_linux_aarch64
+cp target/x86_64-unknown-linux-gnu/release/examples/capa_cli tmp/capa_linux_x86-64
+
+# create the new zip files
+cd tmp
+zip -9r capa-rs"$CURRENT_VER"-windows.zip capa_cli_x86-64.exe
+zip -9r capa-rs"$CURRENT_VER"-macos.zip capa_macos_aarch64 capa_macos_x86-64
+zip -9r capa-rs"$CURRENT_VER"-linux.zip capa_linux_aarch64 capa_linux_x86-64
+cd ..
+
+# delete the tmp files
+rm -f tmp/capa_cli_x86-64.exe tmp/capa_macos_aarch64 tmp/capa_macos_x86-64 tmp/capa_linux_aarch64 tmp/capa_linux_x86-64
